f55568b8fe96c5300b56704a630ff6824f2683b5
bigvillage <woghkstkak@naver.com>
2026-04-23 11:24:14 +0900

    검색 설계

--- task/list/service.js
+++ task/list/service.js
@@ -1,7 +1,8 @@
 // routes/post.js
 const express = require('express')
+const axios = require("axios");
 const router = express.Router()
-const List = require('../upload/models/upload') // 네 mongoose 모델
+const List = require('../upload/models/upload') // mongoose 모델
 
 router.get('/list', async (req, res) => {
   try {
@@ -22,6 +23,44 @@ router.get('/favorites', async (req, res) => {
   } catch (e) {
     console.error(e)
     res.status(500).json({ message: '즐겨찾기 조회 실패' })
+  }
+})
+
+router.get('/search', async (req, res) => {
+  try {
+    const { q } = req.query
+
+    const esRes = await axios.post(
+      `${process.env.ES_URL}/documents/_search`,
+      {
+        query: {
+          wildcard: {
+            title: `*${q}*`
+          }
+        }
+      },
+      {
+        auth: {
+          username: 'elastic',
+          password: "123!@#qwe"
+        }
+      }
+    )
+    console.log("q ==> ", q);
+
+    const hits = esRes.data.hits.hits
+    console.log("hits ==> ", hits);
+
+    const result = hits.map(hit => ({
+      _id: hit._id,
+      ...hit._source
+    }))
+
+    res.json(result)
+
+  } catch (e) {
+    console.error(e.response?.data || e.message)
+    res.status(500).json({ message: '검색 실패' })
   }
 })
 
